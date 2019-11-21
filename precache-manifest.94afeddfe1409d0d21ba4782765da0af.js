f4fcd87d48ad7f58b5855c2dd8ee9f93723244b4
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-21 20:01:00 +0900

    Updates

---
+++ precache-manifest.94afeddfe1409d0d21ba4782765da0af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a164e383f0a94e2c1ad3e46c3ce920d8",
+    "url": "/korean_boxOffice/index.html"
+  },
+  {
+    "revision": "54652fb2bcfff6ea1137",
+    "url": "/korean_boxOffice/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "db8b3ea87a3eac11edf7",
+    "url": "/korean_boxOffice/static/js/2.3c188d2f.chunk.js"
+  },
+  {
+    "revision": "54652fb2bcfff6ea1137",
+    "url": "/korean_boxOffice/static/js/main.37a82b83.chunk.js"
+  },
+  {
+    "revision": "918e0ce4fbc4ba021bdc",
+    "url": "/korean_boxOffice/static/js/runtime-main.b1096df7.js"
+  }
+]);
